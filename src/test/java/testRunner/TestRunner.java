abfe282e6cb0e6dbb0d687b1d0ba630c47f9ff5f
tiktaalick <blogfeedser@gmail.com>
2023-01-18 18:34:17 +0100

    Added Masterthought reporting.

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -7,12 +7,14 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = {"features"},
         glue = {"steps"},
-        plugin = {"pretty", "html:target/reports/Report1.html", "json:target/reports/Report2.json", "junit:target/reports/Report3.xml"},
+        plugin = {"pretty", "html:target/reports/Report1.html", "json:target/json-report/cucumber.json", "junit:target/reports/Report3.xml"},
         dryRun = false,
-        monochrome = true,
+        monochrome = true
+//,
 //        name = {"Home", "Advanced", "iPhone", "Category"},
 //        tags = "@Home or @Advanced or @setCookies"
-        tags = "@setCookies")
+//        tags = "@setCookies"
+)
 public class TestRunner {
 
 }
